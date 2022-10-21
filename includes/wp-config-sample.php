ce6c0218c655ede5e5aa3f745e1792be20611ab0
INTP LLC <info@intp.io>
2022-10-21 16:23:48 -0600

    Mute WP warnings

--- includes/wp-config-sample.php
+++ includes/wp-config-sample.php
@@ -70,7 +70,7 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG', false );
 define( 'WP_DEBUG_LOG', true );
 
 /* That's all, stop editing! Happy publishing. */
